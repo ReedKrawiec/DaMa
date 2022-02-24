8f0a3f7a821ca2748257148a535fe714edb3fa32
Reed B. Krawiec <reedkrawiec@gmail.com>
2022-02-24 17:25:13 -0500

    Initial placement of providers on screenshot

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import os
 import importlib.util
 import sys
 import json
-
+from PIL import Image
+import random
+import copy
 with open("dama.json", "r") as f:
     config = json.loads(f.read())
 
@@ -19,5 +21,19 @@ for provider_name in os.listdir("./providers"):
 images = os.listdir("./small_input")
 
 for index, image in enumerate(images):
-    board = provider_creators["board"]()["image"]
-    board.save(f"test{index}.png")
+    screenshot = Image.open(f"./small_input/{image}")
+    for provider in config["providers"]:
+        name = provider["name"]
+        if random.random() < provider["probability"]:
+            for _ in range(provider["min"],provider["max"]):
+                position = copy.deepcopy(provider["position"])
+                element = provider_creators[name]()
+                if not provider["offscreen"]:
+                    width, height = element["image"].size
+                    position["x"]["max"] = position["x"]["max"] - width
+                    position["y"]["max"] = position["y"]["max"] - height
+                x = random.randrange(position["x"]["min"],position["x"]["max"])
+                y = random.randrange(position["y"]["min"],position["y"]["max"])
+                screenshot.paste(element["image"],(x,y))
+    screenshot.save(f"{index}.png")
+    print(screenshot)
