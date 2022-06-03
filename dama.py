018a07f45ccf9e7a37b5e1fe632dbff79b5663db
Reed Krawiec <reedkrawiec@gmail.com>
2022-06-03 02:24:21 -0400

    lower process count and remove unused code

--- dama.py
+++ dama.py
@@ -8,7 +8,7 @@ import random
 import copy
 from multiprocessing import Pool
 
-PROCESSES = 12
+PROCESSES = 8
 
 DRAW_LABELS = "--draw-labels" in sys.argv
 
@@ -129,8 +129,6 @@ def process(inp):
         )
         folder = distribution[current_group]
         
-        os.makedirs(f"./output/{folder}/images", exist_ok=True)
-        os.makedirs(f"./output/{folder}/labels", exist_ok=True)
         with open(f"./output/{folder}/labels/{int(index + i)}.txt","w") as f:
             classes = len(class_list)
             colors = ["#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
@@ -146,6 +144,7 @@ def process(inp):
                     width = int(width * output["width"])
                     height = int(height * output["height"])
                     draw.rectangle([_x - width/2,_y - height/2,_x + width/2,_y + height/2], outline=colors[cindex])
+        """
         with open(f"./output/classes.txt","w") as f:
             for class_name in class_list:
                 f.write(f"{class_name}\n")
@@ -156,7 +155,7 @@ def process(inp):
             f.write("\n")
             f.write(f"nc: {len(class_list)}\n")
             f.write(f"names: {str(class_list)}")
-        
+        """
         output_image.save(f"./output/{folder}/images/{int(index + i)}.png")
         
         if index > percent * len(images)+ offsets:
@@ -169,101 +168,3 @@ if __name__ == "__main__":
     all_images = list(images)
     with Pool(PROCESSES) as p:
         p.map(process, [(i * len(all_images)/PROCESSES,all_images[i::PROCESSES]) for i in range(PROCESSES)])
-"""
-for index, image in enumerate(images):
-    print("Working on: " + image)
-    screenshot = Image.open(f"{input_dir}/{image}")
-    class_list = []
-    full_labels = []
-    placements = []
-    for provider in config["providers"]:
-        
-        if random.random() < provider["probability"]:
-            for _ in range(provider["min"],provider["max"]):
-                image, labels, width, height, position = generate(provider)
-                counter = 0
-                if not provider["overlap"]:
-                    valid = False
-                    while not valid:
-                        x = random.randrange(position["x"]["min"],position["x"]["max"])
-                        y = random.randrange(position["y"]["min"],position["y"]["max"])
-                        valid = True
-                        for placement in placements:
-                            if squareIntersects((x,y,width,height),placement):
-                                valid = False
-                        counter += 1
-                        if counter > 100:
-                            image, labels, width, height, position = generate(provider)
-                        if counter > 200:
-                            break
-                placements.append((x,y,width,height))
-                # Adjust labels to take into account position within image
-                labels = list(map(lambda label: (label[0] + len(class_list),x + label[1], y + label[2], label[3], label[4]), labels))
-                full_labels.extend(labels)
-                screenshot.paste(image,(int(x),int(y)))
-        class_list.extend(provider["classes"])
-    output_image = Image.new("RGB", (output["width"], output["height"]))
-    if screenshot.size[0] > screenshot.size[1]:
-        
-        ratio = output["width"] / screenshot.size[0]
-        gutter = (output["height"] - screenshot.size[1] * ratio) / 2
-        def adjustlabel(label):
-            class_index, x, y, width, height = label
-            x = int(x * ratio) / output["width"]
-            y = int(y * ratio + gutter) / output["height"] 
-            width = int(width * ratio) / output["width"]
-            height = int(height * ratio) / output["height"]
-            return (class_index, x, y, width, height)
-        # we resize the width to the output width, and the height is proportional
-        screenshot = screenshot.resize((output["width"], int(screenshot.size[1] * ratio)))
-        # adjust labels to account for the resize
-        full_labels = list(map(adjustlabel, full_labels))
-        
-    else:
-        # We resize the size to the output height, and the width is proportional
-        ratio = output["height"] / screenshot.size[1]
-        screenshot = screenshot.resize((int(screenshot.size[0] * ratio), output["height"]))
-    output_image.paste(screenshot, (
-        int((output["width"] - screenshot.size[0]) / 2), # These are the cordinates that place the image in the center of the output image
-        int((output["height"] - screenshot.size[1]) / 2))
-    )
-    folder = distribution[current_group]
-    
-    os.makedirs(f"./output/{folder}/images", exist_ok=True)
-    os.makedirs(f"./output/{folder}/labels", exist_ok=True)
-    with open(f"./output/{folder}/labels/{index}.txt","w") as f:
-        classes = len(class_list)
-        colors = ["#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
-             for i in range(classes)]
-        for label in full_labels:
-            f.write(f"{label[0]} {label[1]} {label[2]} {label[3]} {label[4]}\n")
-            if DRAW_LABELS:
-                draw = ImageDraw.Draw(output_image)
-                cindex,_x,_y,width,height = label
-                _x = int(_x * output["width"])
-                _y = int(_y * output["height"])
-                
-                width = int(width * output["width"])
-                height = int(height * output["height"])
-                draw.rectangle([_x - width/2,_y - height/2,_x + width/2,_y + height/2], outline=colors[cindex])
-    with open(f"./output/classes.txt","w") as f:
-        for class_name in class_list:
-            f.write(f"{class_name}\n")
-        
-    with open(f"./output/data.yaml","w") as f:
-        for dist in distribution:
-            f.write(f"{dist}: ../{dist}/images\n")
-        f.write("\n")
-        f.write(f"nc: {len(class_list)}\n")
-        f.write(f"names: {str(class_list)}")
-    
-    output_image.save(f"./output/{folder}/images/{index}.png")
-    if index > percent * len(images)+ offsets:
-        offsets += int(index)
-        current_group += 1
-        percent = output["distribution"][distribution[current_group]]
-        if current_group > len(distribution):
-            break
-        
-        
-"""
